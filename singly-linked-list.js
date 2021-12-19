97be05f1fdb564e0857759a7227f8298c63e2033
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-12-19 00:58:21 -0800

    singly linked list class

---
+++ singly-linked-list.js
@@ -0,0 +1,49 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    append(val) {
+        if (this.head === null) {
+            this.head = new Node(val)
+            return
+        }
+        let curr = this.head;
+        while (curr.next !== null) {
+            curr = curr.next;
+        }
+        curr.next = new Node(val)
+        return
+    }
+    print() {
+        let str = ''
+        let curr = this.head;
+        while (curr !== null) {
+            str += curr.val + '->';
+            curr = curr.next;
+        }
+        console.log(str)
+    }
+    contains(value) {
+        let curr = this.head;
+        while (curr.next !== null) {
+            if (curr.val === value) return true
+            curr = curr.next;
+        }
+        return false
+    }
+
+}
+
+const list = new LinkedList();
+list.append('a');
+list.append('b');
+console.log(list.contains('x'))
+
+list.print()
